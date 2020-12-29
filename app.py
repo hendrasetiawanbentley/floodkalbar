dc89908760239033e745caf115fe980b931b87c2
hendrasetiawanbentley <74999581+hendrasetiawanbentley@users.noreply.github.com>
2020-12-29 15:14:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,11 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+import dash_daq as daq
+import dash_table
+from dash.exceptions import PreventUpdate
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
