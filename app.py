d13ae62e7037b426ac8f9204af2033799d3b3f60
hendrasetiawanbentley <74999581+hendrasetiawanbentley@users.noreply.github.com>
2020-12-29 15:00:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 
 import dash
 import dash_core_components as dcc
-import dash_html_components as html
-import plotly.express as px
-import pandas as pd
-import dash_table
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
