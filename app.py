3fad1f9cd13f0b2f55930670154412cc8e832179
hendrasetiawanbentley <74999581+hendrasetiawanbentley@users.noreply.github.com>
2020-12-29 16:36:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ app.layout = html.Div([
                                                     {'label': 'SEKADAU', 'value':'SEKADAU'},
                                                     {'label': 'SINTANG', 'value':'SINTANG'},],
                            id='3tahunanlokasi',
-                           value=''),
+                           value='all'),
                           dash_table.DataTable(
                               id='memory-table',
                               columns=[{'name': i, 'id': i} for i in readytgntahun.columns],
@@ -154,7 +154,7 @@ app.layout = html.Div([
                                                     {'label': 'SEKADAU', 'value':'SEKADAU'},
                                                     {'label': 'SINTANG', 'value':'SINTANG'},],
                            id='3tahunanlokasigraph',
-                           value=''),
+                           value='all'),
                     dcc.Graph(id='graptotalperbandingan'),
                     html.P("Ketinggian permukaan pasang mempengaruhi event banjir", style={'textAlign': 'center'})
                     
@@ -286,7 +286,30 @@ def update_hist(name,kabupaten):
 @app.callback(dash.dependencies.Output('memory-output', 'data'),
               dash.dependencies.Input('3tahunanlokasi', 'value'))
 def filter_countries(filterlocation):
-   
+    if filterlocation=='all':
+        # Return all the rows on initial load/no country selected.
+        dfbnbp = pd.read_csv('Data Bencana_bnpb.csv')
+        dfbnbp['Tanggal Kejadian'] = pd.to_datetime(dfbnbp['Tanggal Kejadian'], format="%Y-%m-%d")
+        #create selection for the dataset
+        #untuk histogram feed to the graph
+        #jumlah banjir bulanan kalimantan barat
+        #untuk menampilkan tabel
+        tigathn = dfbnbp['Tanggal Kejadian'].groupby(dfbnbp['Tanggal Kejadian'].dt.to_period("M")).agg('count')
+        tigathn = pd.DataFrame(tigathn)
+        tigathn.columns = ['Jumlah Total Kejadian']
+        tigathn['Bulan Kejadian'] = tigathn.index
+        tigathn['Bulan Kejadian'] =tigathn['Bulan Kejadian'].apply(str)
+        tigathn['Bulan Kejadian'] =pd.to_datetime(tigathn['Bulan Kejadian'], format="%Y-%m")
+        tigathn['year'] = pd.DatetimeIndex(tigathn['Bulan Kejadian']).year
+        tigathn['year']=tigathn['year'].astype(str)
+        tigathn['month'] = pd.to_datetime(tigathn['Bulan Kejadian']).dt.strftime('%b')
+        #tigathn['Bulan kejadian']=tigathn.index
+        tigathn=tigathn.groupby(['month']).sum()
+        readytgntahun=pd.DataFrame(tigathn)
+        readytgntahun['bulan kejadian']=readytgntahun.index
+        readytgntahun=readytgntahun[['bulan kejadian','Jumlah Total Kejadian']]
+        readytgntahun=readytgntahun.sort_values(by='Jumlah Total Kejadian', ascending=False)
+        return readytgntahun.to_dict('records')
 
     #create selection for the dataset
     #untuk histogram feed to the graph
@@ -319,7 +342,6 @@ def on_data_set_table(data):
         raise PreventUpdate
 
     return data
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
