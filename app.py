e32f07cfee61cd404064281c9f0598baf52dc4b6
hendrasetiawanbentley <74999581+hendrasetiawanbentley@users.noreply.github.com>
2020-12-29 15:55:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,24 @@ banjir_count_BNPB = dfbnbp['Tanggal Kejadian'].unique()
 freqbnpb=len(banjir_count_BNPB)
 dfbnbp['Tanggal Kejadian'] = pd.to_datetime(dfbnbp['Tanggal Kejadian'], format="%Y-%m-%d")
 
+tigathn = dfbnbp['Tanggal Kejadian'].groupby(dfbnbp['Tanggal Kejadian'].dt.to_period("M")).agg('count')
+tigathn = pd.DataFrame(tigathn)
+tigathn.columns = ['Jumlah Total Kejadian']
+tigathn['Bulan Kejadian'] = tigathn.index
+tigathn['Bulan Kejadian'] =tigathn['Bulan Kejadian'].apply(str)
+tigathn['Bulan Kejadian'] =pd.to_datetime(tigathn['Bulan Kejadian'], format="%Y-%m")
+tigathn['year'] = pd.DatetimeIndex(tigathn['Bulan Kejadian']).year
+tigathn['year']=tigathn['year'].astype(str)
+tigathn['month'] = pd.to_datetime(tigathn['Bulan Kejadian']).dt.strftime('%b')
+#tigathn['Bulan kejadian']=tigathn.index
+tigathn=tigathn.groupby(['month']).sum()
+readytgntahun=pd.DataFrame(tigathn)
+readytgntahun['bulan kejadian']=readytgntahun.index
+readytgntahun=readytgntahun[['bulan kejadian','Jumlah Total Kejadian']]
+readytgntahun=readytgntahun.sort_values(by='Jumlah Total Kejadian', ascending=False)
+bulanbanjir = px.bar(readytgntahun,x='bulan kejadian',y='Jumlah Total Kejadian',color="bulan kejadian")
+bulanbanjir.update_xaxes(type='category',tickmode='linear')
+
 app.layout = html.Div([
     html.H2('Hello World'),
     dcc.Dropdown(
