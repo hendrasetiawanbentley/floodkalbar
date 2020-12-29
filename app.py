7c93a0eec55edb2ce92f81115c9d833327d8953d
hendrasetiawanbentley <74999581+hendrasetiawanbentley@users.noreply.github.com>
2020-12-29 16:39:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -283,8 +283,8 @@ def update_hist(name,kabupaten):
     newfig.update_layout(paper_bgcolor='rgba(0,0,0,0)',plot_bgcolor='rgba(0,0,0,0)')
     return newfig 
 
-@app.callback(dash.dependencies.Output('memory-output', 'data'),
-              dash.dependencies.Input('3tahunanlokasi', 'value'))
+@app.callback(dash.dependencies.Output('graptotalperbandingan', 'figure'),
+              dash.dependencies.Input('3tahunanlokasigraph', 'value'))
 def filter_countries(filterlocation):
     if filterlocation=='all':
         # Return all the rows on initial load/no country selected.
@@ -309,7 +309,10 @@ def filter_countries(filterlocation):
         readytgntahun['bulan kejadian']=readytgntahun.index
         readytgntahun=readytgntahun[['bulan kejadian','Jumlah Total Kejadian']]
         readytgntahun=readytgntahun.sort_values(by='Jumlah Total Kejadian', ascending=False)
-        return readytgntahun.to_dict('records')
+        bulanbanjir = px.bar(readytgntahun,x='bulan kejadian',y='Jumlah Total Kejadian',color="bulan kejadian")
+        bulanbanjir.update_xaxes(type='category',tickmode='linear')
+        bulanbanjir.update_layout(paper_bgcolor='rgba(0,0,0,0)',plot_bgcolor='rgba(0,0,0,0)')
+        return bulanbanjir
 
     #create selection for the dataset
     #untuk histogram feed to the graph
@@ -333,15 +336,10 @@ def filter_countries(filterlocation):
     readytgntahun['bulan kejadian']=readytgntahun.index
     readytgntahun=readytgntahun[['bulan kejadian','Jumlah Total Kejadian']]
     readytgntahun=readytgntahun.sort_values(by='Jumlah Total Kejadian', ascending=False)
-    return readytgntahun.to_dict('records')
-
-@app.callback(dash.dependencies.Output('memory-table', 'data'),
-              dash.dependencies.Input('memory-output', 'data'))
-def on_data_set_table(data):
-    if data is None:
-        raise PreventUpdate
-
-    return data
+    bulanbanjir = px.bar(readytgntahun,x='bulan kejadian',y='Jumlah Total Kejadian',color="bulan kejadian")
+    bulanbanjir.update_xaxes(type='category',tickmode='linear')
+    bulanbanjir.update_layout(paper_bgcolor='rgba(0,0,0,0)',plot_bgcolor='rgba(0,0,0,0)')
+    return bulanbanjir
 
 if __name__ == '__main__':
     app.run_server(debug=True)
